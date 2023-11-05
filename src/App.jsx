fc164d16e6e5d20886c60272e34f480ed732d71e
Nasih <iamnasih@gmail.com>
2023-11-05 21:16:57 +0530

    input layout added

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,18 @@ function App() {
   return (
    <div className='w-full max-w-md mx-auto shadow-md 
    rounded-lg px-4 py-4 my-8 bg-gray-800 text-orange-500'>
-
+    <h1 className='text-3xl font-bold mb-2
+    text-center'>Password Generator</h1>
+    <div className='flex shadow rounded-lg overflow-hidden
+    mb-4'>
+      <input type="text"
+      value={password}
+      className='outline-none w-full py-1 px-3'
+      placeholder='Password' 
+      readOnly/>
+      <button className='outline-none bg-blue-700 text-white px-4
+      py-0.5 shrink-0'>copy</button>
+    </div>
    </div>
   )
 }
