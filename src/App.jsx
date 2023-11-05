1f0e57d14068432c16d18d8207a46846e672c5c3
Nasih <iamnasih@gmail.com>
2023-11-05 22:00:43 +0530

    method for generating password added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +8,22 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState('');
+
+  const generatePassword = useCallback(()=>{
+    let pass = ""
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+"
+    
+    for(let i = 1; i < length; i++){
+     const char =  Math.floor(Math.random() * str.length + 1)
+     pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed])
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md 
